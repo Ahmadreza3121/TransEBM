4a3b3968b8625d1e07965c834843a8d8adb5b461
Ahmadreza3121 <ahmadreza.masoum@awi.de>
2021-01-23 12:33:19 +0100

    status day3_compute Tsurf

--- config/config_module.sh
+++ config/config_module.sh
@@ -2,7 +2,7 @@
 
 # changes to run the model:
 # 1) change compiler ifort to compiler of choice
-ifort -c parse_config.f90 -o ../config/parse_config.o
+gfortran-7 -c parse_config.f90 -o ../config/parse_config.o
 
 cp parse_config.o ../preprocess/
 cp parse_config.o ../src/
--- src/Lapsrate_diffusion.f90
+++ src/Lapsrate_diffusion.f90
@@ -34,7 +34,7 @@ do i = 1, nx
   do j = 1, ny
     !Temp(i,j) = 10.
     Tsurf(i,j) = Temp(i,j) - orography(i,j) * Laps_rate
-    write(*,*) i,j,Tsurf(i,j),Temp(i,j),orography(i,j)                         
+    !write(*,*) i,j,Tsurf(i,j),Temp(i,j),orography(i,j)                         
   end do
 end do  
 
